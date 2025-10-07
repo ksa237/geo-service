bff449f2a577403d4f0f3d79093d797956ae4a0f
ksa237 <ksa237@yandex.ru>
2025-10-07 23:00:40 +1000

    ver03

---
+++ src/test/java/ru/netology/sender/MessageSenderImplTest.java
@@ -0,0 +1,43 @@
+package ru.netology.sender;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import ru.netology.entity.Country;
+import ru.netology.entity.Location;
+import ru.netology.geo.GeoService;
+import ru.netology.geo.GeoServiceImpl;
+import ru.netology.i18n.LocalizationService;
+import ru.netology.i18n.LocalizationServiceImpl;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class MessageSenderImplTest {
+
+    @Test
+    void send_rusIP_receive_rusAnswerTest() {
+
+        GeoService geoService = Mockito.mock(GeoServiceImpl.class);
+        LocalizationService localizationService = Mockito.mock(LocalizationServiceImpl.class);
+
+        MessageSender messageSender = new MessageSenderImpl(geoService, localizationService);
+
+        //geoService.byIp возвращает Location, создадим нужный объект этого вида.
+        Location russianLocation = new Location("Moscow", Country.RUSSIA, "Lenina", 15);
+
+        //зададим поведение для методов mock-классов
+
+        Mockito.when(geoService.byIp(Mockito.any())).thenReturn(russianLocation);
+        Mockito.when(localizationService.locale(Mockito.any())).thenReturn("Добро пожаловать");
+
+        //вспомогательные входящие параметры
+        Map<String, String> headersRus = new HashMap<>();
+        headersRus.put(MessageSenderImpl.IP_ADDRESS_HEADER, "172.0.32.11");
+
+        String answer = messageSender.send(headersRus);
+        String expected = "Добро пожаловать";
+        Assertions.assertEquals(expected, answer);
+
+    }
+}
